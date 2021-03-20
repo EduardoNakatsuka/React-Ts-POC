08b3e993c1b04e0e7cb07b78f5a0a14d3caae9e9
Eduardo Silva <edu_godoys@hotmail.com>
2021-03-20 08:24:15 -0300

    feature(CatList): Add CatList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { CatList } from './Pages/CatList';
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <>
+          <CatList />
         </>
       </header>
     </div>
---
+++ src/Pages/CatList.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+// interface CatObject {
+//   name: string,
+//   color?: string,
+// } TODO
+
+export const CatList: React.FC = () => {
+  const [catName, setCatName] = useState<string>('');
+  const [cats, setCats] = useState<string[]>([]);
+
+  const onCatNameChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setCatName(e.target.value);
+  };
+
+  // const addCat = (e: React.MouseEvent<HTMLButtonElement>): void => { // not going to use event sooo
+  const addCat = (): void => {
+    setCatName('');
+    setCats([...cats, catName]);
+  };
+
+  const checkAddCat = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter') addCat();
+  };
+
+  return (
+    <div>
+      <h3>Cat List</h3>
+
+      <ul>
+        {cats.map((cat, index) => (
+          <li key={index}>
+            {cat}
+          </li>
+        ))}
+      </ul>
+
+      <input value={catName} onChange={onCatNameChange} onKeyDown={checkAddCat} />
+      <button onClick={addCat}>Add cat</button>
+    </div>
+  )
+}
