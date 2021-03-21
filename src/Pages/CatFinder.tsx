ac3a4f0cd0836ed82dafe482170c9f436b41485c
Eduardo Silva <edu_godoys@hotmail.com>
2021-03-20 23:56:43 -0300

    feature(CatFinder): Add ref

--- src/Pages/CatFinder.tsx
+++ src/Pages/CatFinder.tsx
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+interface Cats {
+  cats: Cat[];
+};
 
 const cats = [
   { name: 'Pitu', color: 'brown' },
@@ -13,8 +17,15 @@ interface Cat {
 };
 
 export const CatFinder: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [catName, setCatName] = useState<string>('');
   const [foundCat, setFoundCat] = useState<Cat | undefined>();
+
+  useEffect(() => {
+    if (!inputRef.current) return;
+
+    inputRef.current.focus();
+  }, []);
 
   const findCat = (): void => {
     const foundCat = cats.find(cat => {
@@ -28,7 +39,11 @@ export const CatFinder: React.FC = () => {
     <div>
       <h3>Find Cat</h3>
 
-      <input value={catName} onChange={e => setCatName(e.target.value)} />
+      <input
+        ref={inputRef}
+        value={catName}
+        onChange={e => setCatName(e.target.value)}
+      />
 
       <button onClick={findCat}>Find Cat</button>
 
