2c645c8baeceac1c2a6da5b5bc2e3e7f86fd9a7c
Eduardo Silva <edu_godoys@hotmail.com>
2021-03-23 09:23:57 -0300

    refactor(RepositoriesList): Apply useSelector

--- src/Pages/RepositoriesList.tsx
+++ src/Pages/RepositoriesList.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useActions } from '../hooks/useActions';
 
 export const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  const state = useSelector((state) => state);
 
   const searchRepository = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
